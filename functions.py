5cebf92a7621c23c9c3772bb638bad407a2da941
correajp <jpcorread@uc.cl>
2022-08-17 20:48:49 -0400

    dias mas tweeteados

--- functions.py
+++ functions.py
@@ -1,3 +1,6 @@
+from datetime import date
+
+
 def most_tweeted(tweets):
     mas_retweeteados = sorted(tweets, key=lambda d: d['retweetCount'])
     mas_retweeteados = mas_retweeteados[-10:]
@@ -7,8 +10,7 @@ def top_users(tweets):
     tweeteros = dict()
     for tweet in tweets:
         if tweet['user']['id'] not in tweeteros.keys():
-            tweeteros[tweet['user']['id']] = 0
-        else: 
+            tweeteros[tweet['user']['id']] = 1
             tweeteros[tweet['user']['id']] += 1
     tweeteros = sorted(tweeteros.items(), key=lambda x: x[1], reverse=True)
     tweeteros = tweeteros[0:10]
@@ -25,7 +27,19 @@ def top_users(tweets):
     
 
 def top_days(tweets):
-    pass
+    dias = dict()
+    for tweet in tweets:
+        if str(tweet['date'][0:10]) not in dias.keys():
+            dias[str(tweet['date'][0:10])] = 1
+        else:
+            dias[str(tweet['date'][0:10])] += 1
+
+    dias = sorted(dias.items(), key=lambda x: x[1], reverse=True)
+    dias = dias[0:10]
+
+    return dias
+
+ 
 
 def top_hashtags(tweets):
     pass
--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import json 
-from functions import most_tweeted, top_users
+from functions import most_tweeted, top_users, top_days, top_hashtags
 
 if __name__ == '__main__':
     tweets = []
     for line in open('farmers-protest-tweets-2021-03-5.json', 'r'):
         tweets.append(json.loads(line))
 
-    print(most_tweeted(tweets))
-    print(top_users(tweets))
+    # print(most_tweeted(tweets))
+    # print(top_users(tweets))
+    print(top_days(tweets))
 
   
